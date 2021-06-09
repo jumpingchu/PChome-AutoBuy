fc55e9d130f7d5ec35e6c374a35bb3c1084cdc91
jumpingchu <jumpingchu@gmail.com>
2021-06-09 11:46:26 +0800

    Add settings file

---
+++ settings.py
@@ -0,0 +1,9 @@
+URL = "商品連結"
+CHROME_PATH=r"--user-data-dir=C:\\<chrome 設定檔路徑>"  # 可透過網址列輸入 chrome://version/ 找到
+
+# 請注意！以下皆為機密個資，請小心謹慎，勿上傳至公開平台
+ACC = "PChome 登入帳號"
+PWD = "PChome 登入密碼"
+BuyerSSN = "身分證字號"
+BirthYear, BirthMonth, BirthDay = "出生年", "月", "日"
+multi_CVV2Num = "信用卡檢查碼（背面三碼）"
