bc71660742de279c64e0836989f91b85eec92e64
jumpingchu <jumpingchu@gmail.com>
2021-06-23 15:38:52 +0800

    add DRIVER_PATH for Mac

--- settings.py
+++ settings.py
@@ -1,9 +1,14 @@
 URL = "商品連結"
-CHROME_PATH=r"--user-data-dir=C:\\<chrome 設定檔路徑>"  # 可透過網址列輸入 chrome://version/ 找到
+DRIVER_PATH = "chromedriver.exe"
+CHROME_PATH = r"--user-data-dir=C:\\<chrome 設定檔路徑>"  # 可透過網址列輸入 chrome://version/ 找到
+
+### Only for Mac ###
+# DRIVER_PATH = "/usr/local/bin/chromedriver"
+### Only for Mac (End) ###
 
 # 請注意！以下皆為機密個資，請小心謹慎，勿上傳至公開平台
 ACC = "PChome 登入帳號"
 PWD = "PChome 登入密碼"
 BuyerSSN = "身分證字號"
-BirthYear, BirthMonth, BirthDay = "出生年", "月", "日"
+BirthYear, BirthMonth, BirthDay = "民國出生年", "月", "日"
 multi_CVV2Num = "信用卡檢查碼（背面三碼）"
