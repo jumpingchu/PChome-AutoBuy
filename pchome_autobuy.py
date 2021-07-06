16700d1570ec09ae20a3f3b708f7eff2fc0d3d45
jumpingchu <jumpingchu@gmail.com>
2021-07-06 22:29:22 +0800

    add input_flow function

--- pchome_autobuy.py
+++ pchome_autobuy.py
@@ -16,7 +16,8 @@ from selenium.webdriver.common.by import By
 匯入欲搶購的連結、登入帳號、登入密碼及其他個資
 """
 from settings import (
-    URL, DRIVER_PATH, CHROME_PATH, ACC, PWD, BuyerSSN, BirthYear, BirthMonth, BirthDay, multi_CVV2Num    
+    URL, DRIVER_PATH, CHROME_PATH, ACC, PWD,
+    BuyerSSN, BirthYear, BirthMonth, BirthDay, multi_CVV2Num    
 )
 
 def login():
@@ -51,6 +52,20 @@ def click_button(xpath):
     )
     driver.find_element_by_xpath(xpath).click()
 
+def input_flow():
+    """
+    填入個資，若無法填入則直接填入信用卡背面安全碼 3 碼 (multi_CVV2Num)
+    """
+    try:
+        input_info(xpaths['BuyerSSN'], BuyerSSN)
+        input_info(xpaths['BirthYear'], BirthYear)
+        input_info(xpaths['BirthMonth'], BirthMonth)
+        input_info(xpaths['BirthDay'], BirthDay)
+    except:
+        pass
+    finally:
+        input_info(xpaths['multi_CVV2Num'], multi_CVV2Num)
+
 def get_product_id(url):
     pattern = '(?<=prod/)(\w+-\w+)'
     try:
@@ -84,96 +99,78 @@ xpaths = {
 }
 
 def main():
+    driver.get(URL)
+
+    """
+    放入購物車
+    """
+    click_button(xpaths['add_to_cart'])
+
+    """
+    前往購物車
+    """
+    driver.get("https://ecssl.pchome.com.tw/sys/cflow/fsindex/BigCar/BIGCAR/ItemList")
+
+    """
+    登入帳戶（注意！若有使用 CHROME_PATH 記住登入資訊，第二次執行時請記得註解掉登入這行！）
+    """
+    login()
+
+    """
+    前往結帳 (一次付清) (要使用 JS 的方式 execute_script 點擊)
+    """
+    WebDriverWait(driver, 20).until(
+        expected_conditions.element_to_be_clickable(
+            (By.XPATH, "//li[@class='CC']/a[@class='ui-btn']"))
+    )
+    button = driver.find_element_by_xpath(
+        "//li[@class='CC']/a[@class='ui-btn']")
+    driver.execute_script("arguments[0].click();", button)
+
+    """
+    LINE Pay 付款
+    """
+    # WebDriverWait(driver, 20).until(
+    #     expected_conditions.element_to_be_clickable(
+    #         (By.XPATH, "//li[@class='LIP']/a[@class='ui-btn line_pay']"))
+    # )
+    # button = driver.find_element_by_xpath(
+    #     "//li[@class='LIP']/a[@class='ui-btn line_pay']")
+    # driver.execute_script("arguments[0].click();", button)
+
+    """
+    點擊提示訊息確定 (有些商品可能不需要)
+    """
     try:
-        driver.get(URL)
-
-        """
-        放入購物車
-        """
-        WebDriverWait(driver, 20).until(
+        WebDriverWait(driver, 1).until(
             expected_conditions.element_to_be_clickable(
-                (By.XPATH, "//li[@id='ButtonContainer']/button"))
+                (By.XPATH, "//a[@id='warning-timelimit_btn_confirm']"))
         )
-        driver.find_element_by_xpath("//li[@id='ButtonContainer']/button").click()
-
-        """
-        前往購物車
-        """
-        driver.get("https://ecssl.pchome.com.tw/sys/cflow/fsindex/BigCar/BIGCAR/ItemList")
-
-        """
-        登入帳戶（注意！若有使用 CHROME_PATH 記住登入資訊，第二次執行時請記得註解掉登入這行！）
-        """
-        login()
-
-        """
-        前往結帳 (一次付清) (要使用 JS 的方式 execute_script 點擊)
-        """
-        WebDriverWait(driver, 20).until(
-            expected_conditions.element_to_be_clickable(
-                (By.XPATH, "//li[@class='CC']/a[@class='ui-btn']"))
-        )
-        button = driver.find_element_by_xpath(
-            "//li[@class='CC']/a[@class='ui-btn']")
+        button = driver.find_element_by_xpath("//a[@id='warning-timelimit_btn_confirm']")
         driver.execute_script("arguments[0].click();", button)
+    except:
+        pass
 
-        """
-        LINE Pay 付款
-        """
-        # WebDriverWait(driver, 20).until(
-        #     expected_conditions.element_to_be_clickable(
-        #         (By.XPATH, "//li[@class='LIP']/a[@class='ui-btn line_pay']"))
-        # )
-        # button = driver.find_element_by_xpath(
-        #     "//li[@class='LIP']/a[@class='ui-btn line_pay']")
-        # driver.execute_script("arguments[0].click();", button)
+    """
+    填入個資
+    """
+    input_flow()
 
-        """
-        點擊提示訊息確定 (有些商品可能不需要)
-        """
-        try:
-            WebDriverWait(driver, 1).until(
-                expected_conditions.element_to_be_clickable(
-                    (By.XPATH, "//a[@id='warning-timelimit_btn_confirm']"))
-            )
-            button = driver.find_element_by_xpath("//a[@id='warning-timelimit_btn_confirm']")
-            driver.execute_script("arguments[0].click();", button)
-        except:
-            pass
+    """
+    勾選同意（注意！若帳號有儲存付款資訊的話，不需要再次勾選，請註解掉！）
+    """
+    click_button(xpaths['check_agree'])
 
-        """
-        填入個資（注意！若帳號有儲存付款資訊的話，不需要再次填入身分證字號和出生年月日，可註解掉直接進行信用卡後三碼！）
-        """
-        try:
-            input_info(xpaths['BuyerSSN'], BuyerSSN)
-            input_info(xpaths['BirthYear'], BirthYear)
-            input_info(xpaths['BirthMonth'], BirthMonth)
-            input_info(xpaths['BirthDay'], BirthDay)
-        except:
-            pass
+    """
+    送出訂單 (要使用 JS 的方式 execute_script 點擊)
+    """
+    WebDriverWait(driver, 20).until(
+        expected_conditions.element_to_be_clickable(
+            (By.XPATH, "//a[@id='btnSubmit']"))
+    )
+    button = driver.find_element_by_xpath("//a[@id='btnSubmit']")
+    driver.execute_script("arguments[0].click();", button)
 
-        """
-        填入信用卡背面安全碼 3 碼 (multi_CVV2Num)
-        """
-        input_info(xpaths['multi_CVV2Num'], multi_CVV2Num)
-
-        """
-        勾選同意（注意！若帳號有儲存付款資訊的話，不需要再次勾選，請註解掉！）
-        """
-        click_button(xpaths['check_agree'])
-
-        """
-        送出訂單 (要使用 JS 的方式 execute_script 點擊)
-        """
-        WebDriverWait(driver, 20).until(
-            expected_conditions.element_to_be_clickable(
-                (By.XPATH, "//a[@id='btnSubmit']"))
-        )
-        button = driver.find_element_by_xpath("//a[@id='btnSubmit']")
-        driver.execute_script("arguments[0].click();", button)
-
-    except Exception as e:
-        print(e)
 
 """
 設定 option 可讓 chrome 記住已登入帳戶，成功後可以省去後續"登入帳戶"的程式碼
